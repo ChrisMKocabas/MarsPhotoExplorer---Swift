6ac94431ad9cee7cdfb11da9a1f404f24544dfb8
ChrisMKocabas <muhammedkocabas@outlook.com>
2023-02-04 17:31:45 -0500

    Initial Commit

---
+++ MarsPhotoExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarsPhotoExplorer
+//
+//  Created by Muhammed Kocabas on 2023-02-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
